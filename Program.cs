03ea9a7a86210ec35c10f83cfcdb4731a90225bd
wicho1001 <wichoguerrero@yahoo.com>
2019-07-18 22:46:53 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace cajero_automatico
+{
+    class Program
+    {
+        static 
+        void Billetes(string cantidad)
+        {
+            int[] BilletesDeCien = {100, 100, 100, 100, 100, 100};
+            int[] BilletesDeDoscientos = {200, 200, 200, 200, 200, 200};
+            int[] BilletesDeQuinientos = {500, 500, 500, 500, 500, 500};
+            int[] BilletesDeMil = {1000, 1000, 1000, 1000, 1000, 1000};
+            Console.WriteLine("Hello World desde billetes!"); 
+            Console.WriteLine("Esta es la cantidad {0}", cantidad);
+            Console.WriteLine(BilletesDeCien[0]);
+        }
+        static void Main(string[] args)
+        {
+            string cantidad = "";
+            Console.WriteLine("Ingresa la cantidad que deseas: ");
+            cantidad = Console.ReadLine();
+            Billetes(cantidad);
+
+        }
+    }
+}
