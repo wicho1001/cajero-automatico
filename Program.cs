a22438a33d0c9d0929af27a6a841b0dbb4c74850
wicho1001 <wichoguerrero@yahoo.com>
2019-07-26 00:25:01 -0500

    Fix: Falta arreglar el while y separar las funciones

--- Program.cs
+++ Program.cs
@@ -1,25 +1,59 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace cajero_automatico
 {
     class Program
     {
-        static 
-        void Billetes(string cantidad)
+        static void Billetes(string cantidad)
         {
-            int[] BilletesDeCien = {100, 100, 100, 100, 100, 100};
-            int[] BilletesDeDoscientos = {200, 200, 200, 200, 200, 200};
-            int[] BilletesDeQuinientos = {500, 500, 500, 500, 500, 500};
-            int[] BilletesDeMil = {1000, 1000, 1000, 1000, 1000, 1000};
+            Stack<int> BilletesDeCien = new Stack<int>();
+            Stack<int> BilletesDeDoscientos = new Stack<int>();
+            Stack<int> BilletesDeQuinientos = new Stack<int>();
+            Stack<int> BilletesDeMil = new Stack<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                BilletesDeCien.Push(100);
+                BilletesDeDoscientos.Push(200);
+                BilletesDeQuinientos.Push(500);
+                BilletesDeMil.Push(1000);
+            }
             Console.WriteLine("Hello World desde billetes!"); 
             Console.WriteLine("Esta es la cantidad {0}", cantidad);
-            Console.WriteLine(BilletesDeCien[0]);
+            int cantidadInt;
+            cantidadInt = Convert.ToInt32(cantidad);
+            if (cantidadInt == 100)
+            {
+                BilletesDeCien.Pop();
+            } if (cantidadInt == 200)
+            {
+                BilletesDeDoscientos.Pop();
+            } if (cantidadInt == 500)
+            {
+                BilletesDeQuinientos.Pop();
+            } if (cantidadInt == 1000)
+            {
+                BilletesDeMil.Pop();
+            } 
+            int[] BilletesDeCienArr = BilletesDeCien.ToArray();
+            int[] BilletesDeDoscientosArr = BilletesDeDoscientos.ToArray();
+            int[] BilletesDeQuinientosArr = BilletesDeQuinientos.ToArray();
+            int[] BilletesDeMilArr = BilletesDeMil.ToArray();
+
+            Console.WriteLine("Quedan {0} billetes de 100",BilletesDeCienArr.Length);
+            Console.WriteLine("Quedan {0} billetes de 200",BilletesDeDoscientosArr.Length);
+            Console.WriteLine("Quedan {0} billetes de 500",BilletesDeQuinientosArr.Length);
+            Console.WriteLine("Quedan {0} billetes de 1000",BilletesDeMilArr.Length);
+            while (BilletesDeCienArr.Length > 0)
+            {
+                Main();
+            }
         }
             // Lo que estoy pensando aqui es hacer el void para la asignacion de los billetes hacia la cantidad haciendo operaciones 
             // sobre la cantidad y que me vaya eliminando elementos de mi array a traves de un pop
 
             // Como pensaba hacerlo era a traves de condicionantes if else if else pero siento que se hara muy extenso el codigo
-        static void Main(string[] args)
+        static void Main()
         {
             string cantidad = "";
             Console.WriteLine("Ingresa la cantidad que deseas: ");
